4b44c5b8f4373bdf5af8701b635c5809bc2c3093
Lorenzo Rossi <lorenzo.rossi-4@studenti.unitn.it>
2022-11-03 16:05:09 +0100

    Certs in config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,39 @@
 #if [ -f /mosquitto/config/mosquitto.conf ]; then
 #    echo 'CONFIG file exists. SKIPPING.'
 #else
-  cp /src/mosquitto.conf /mosquitto/config/mosquitto.conf
 #fi
 
 #if [ -f /mosquitto/config/passwd ]; then
 #    echo 'passwd file exists. SKIPPING.'
 #else
-  cp /src/passwd /mosquitto/config/passwd
 #fi
 
+echo "Copying mosquitto.conf"
+cp /src/mosquitto.conf /mosquitto/config/mosquitto.conf
+echo "Copying passwd"
+cp /src/passwd /mosquitto/config/passwd
+
+if [ -f /mosquitto/config/certs ]; then
+	echo "Certs' folder already exists. SKIPPING."
+else
+	echo "Creating certs' directory"
+	mkdir /mosquitto/config/certs
+fi
+
+if [ -f /mosquitto/config/ca_certs ]; then
+	echo "CA Certs' folder already exists. SKIPPING."
+else
+	echo "Creating CA certs' directory"
+	mkdir /mosquitto/config/certs
+fi
+
+echo "Copying broker.crt"
 cp /src/broker.crt /mosquitto/config/certs/broker.crt
+echo "Copying broker.key"
 cp /src/broker.key /mosquitto/config/certs/broker.key
+echo "Copying ca.crt"
 cp /src/ca.crt /mosquitto/config/ca_certs/ca.crt
+
+echo "Done"
 
 exec "$@"
